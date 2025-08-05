dc3c1e80c485217b485cc6360b93bd495f63f959
tanishq1172005 <tanishq1172005@gmail.com>
2025-08-05 21:42:27 +0530

    New change

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -12,33 +12,31 @@ export default function Landing(){
     <Banner/>
     <div className="flex space-x-5 justify-center mt-6">
         <Button>Donate now</Button>
-        <Button>See blood Availability</Button>
     </div>
     <div className="flex flex-col items-center">
-        <h1 className="font-bold mt-10 md:text-xl xl:text-2xl">Why RaktMittra?</h1>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-2 p-4">
-      <Card 
-        icon={<FaHospital />} 
-        title="For Hospitals" 
-        description="Seamlessly manage blood bookings, track availability, and handle emergency requests with ease."
-        actionText="Learn More"
-        onAction={() => alert("Hospitals Info")}
-      />
-      <Card 
-        icon={<FaUserMd />} 
-        title="For Patients" 
-        description="View nearby blood banks and stay informed about emergency fallback systems in real time."
-        actionText="Learn More"
-        onAction={() => alert("Patients Info")}
-      />
-      <Card 
-        icon={<FaHeart />} 
-        title="For Donors" 
-        description="Find donation centers near you and receive non-cash incentives for your life-saving contribution."
-        actionText="Learn More"
-        onAction={() => alert("Donors Info")}
-      />
+        <h1 className="font-bold mt-10 md:text-xl xl:text-2xl">Why RaktMittra?</h1></div>
+      <div className="mx-2 grid grid-cols-1 md:grid-cols-3 gap-2 p-4">
+        <Card 
+          icon={<FaHospital />} 
+          title="For Hospitals" 
+          description="Seamlessly manage blood bookings, track availability, and handle emergency requests with ease."
+          actionText="Learn More"
+          onAction={() => alert("Hospitals Info")}
+        />
+        <Card 
+          icon={<FaUserMd />} 
+          title="For Patients" 
+          description="View nearby blood banks and stay informed about emergency fallback systems in real time."
+          actionText="Learn More"
+          onAction={() => alert("Patients Info")}
+        />
+        <Card 
+          icon={<FaHeart />} 
+          title="For Donors" 
+          description="Find donation centers near you and receive non-cash incentives for your life-saving contribution."
+          actionText="Learn More"
+          onAction={() => alert("Donors Info")}
+        />
     </div>  
-    </div>
     </>)
 }
